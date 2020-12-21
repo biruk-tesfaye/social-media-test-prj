b15931eb3918e0e8cd87f4ed15dd260383d10e07
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2020-12-21 20:24:49 +0300

    list users

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
 import Home from "../src/components/home/home";
 import Layout from "../src/components/_shared/Layout/layout";
-export default function HomePage() {
+import fetch from "isomorphic-unfetch";
+
+export type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: any;
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+export type HomeType = {
+  users: UserType[];
+};
+
+export default function HomePage({ users }: HomeType) {
   return (
     <Layout>
-      <Home />
+      <Home users={users} />
     </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const data = await fetch(
+    `https://jsonplaceholder.typicode.com/users`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import { HomeType } from "../../../pages";
+import styles from "./home.module.scss";
+import UserCard from "../_shared/user-card/user-card";
+
+export default function Home({ users }: HomeType) {
   return (
-    <div>
-      <h1>This is Home page</h1>
+    <div className={styles["home"]}>
+      <div className={styles["lead"]}>
+        <h1>Social Media Users List</h1>
+      </div>
+
+      <div className={styles['card-grid-container']}>
+        {users.map((user) => (
+          <UserCard user={user} />
+        ))}
+      </div>
     </div>
   );
 }
